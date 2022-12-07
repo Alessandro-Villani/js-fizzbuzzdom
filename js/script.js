f5da72f47abb8071f2a2d9aed9caf07960e98b44
Alessandro-Villani <alessvill@libero.it>
2022-12-07 15:26:59 +0100

    created loop

--- js/script.js
+++ js/script.js
@@ -34,3 +34,9 @@ console.log('JS OK');
 4. If all the above is false print the number
 
 ----------------------------------------------------------------------------------------*/
+
+//1. Create loop from 1 to 100
+
+for ( i = 1; i <= 100; i++){
+    console.log(i);
+}
